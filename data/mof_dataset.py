6e96955559e68d0d902d4198a078ad99b153fbb2
NaYoungKim <lkny123@kaist.ac.kr>
2024-07-03 17:03:19 +0900

    dataset for MOF

---
+++ data/mof_dataset.py
@@ -0,0 +1,405 @@
+"""MOF dataset loader."""
+import math
+from typing import Optional
+
+import torch
+import torch.distributed as dist
+
+import tree
+import numpy as np
+import torch
+import pandas as pd
+import logging
+import random
+import functools as fn
+
+from torch.utils import data
+from torch_geometric.data import Data
+
+from data import utils as du
+
+from openfold.utils import rigid_utils
+from ase.data import chemical_symbols
+
+from pymatgen.core.structure import Structure
+from pymatgen.core.lattice import Lattice
+from pymatgen.analysis.graphs import StructureGraph
+from pymatgen.analysis import local_env
+
+from mofdiff.common.data_utils import frac_to_cart_coords
+
+
+CrystalNN = local_env.CrystalNN(
+    distance_cutoffs=None, x_diff_weight=-1, porous_adjustment=False)
+
+class MOFDataset(data.Dataset):
+    def __init__(
+            self,
+            *,
+            data_conf,
+            diffuser,
+            is_training,
+        ):
+
+        self.cached_data = torch.load(data_conf.cache_path)
+
+        self._log = logging.getLogger(__name__)
+        self._is_training = is_training
+        self._data_conf = data_conf
+        self._diffuser = diffuser
+
+    @property
+    def is_training(self):
+        return self._is_training
+
+    @property
+    def diffuser(self):
+        return self._diffuser
+
+    @property
+    def data_conf(self):
+        return self._data_conf
+
+    def get_rigids_0(self, mof):
+
+        rigids_0 = torch.zeros((mof.num_components, 7))
+
+        R = np.eye(3)
+        quat = torch.tensor(du.rotvec_to_quat(du.matrix_to_rotvec(R)), dtype=torch.float32)
+
+        for i, bb in enumerate(mof.pyg_mols):
+            x = frac_to_cart_coords(bb.frac_coords, mof.lengths, mof.angles, bb.num_atoms)
+
+            # Center the coordinates
+            centroid = x.mean(dim=0)
+            bb.pos = x - centroid
+
+            rigids_0[i] = torch.cat([quat, centroid])
+
+        return rigids_0 
+
+    def __len__(self):
+        return len(self.cached_data)
+
+    def __getitem__(self, idx):
+
+        data = self.cached_data[idx]
+
+        rigids_0 = self.get_rigids_0(data)
+
+        # Use a fixed seed for evaluation.
+        if self.is_training:
+            rng = np.random.default_rng(None)
+        else:
+            rng = np.random.default_rng(idx)
+
+        # Sample t and diffuse.
+        gt_bb_rigid = rigid_utils.Rigid.from_tensor_7(rigids_0)
+        if self.is_training:
+            t = rng.uniform(self._data_conf.min_t, 1.0)
+            diff_feats_t = self._diffuser.forward_marginal(
+                rigids_0=gt_bb_rigid,
+                t=t,
+                diffuse_mask=None
+            )
+        else:
+            t = 1.0
+            diff_feats_t = self.diffuser.sample_ref(
+                n_samples=gt_bb_rigid.shape[0],
+                impute=gt_bb_rigid,
+                diffuse_mask=None,
+                as_tensor_7=True,
+            )
+
+        ######### CONSTRUCT GRAPH #########
+        lattice = torch.cat([data.lengths, data.angles]).numpy().flatten()
+
+        # get coordinates, atom_types
+        x_t = []
+        atom_types = []
+        _rigids_t = rigid_utils.Rigid.from_tensor_7(diff_feats_t['rigids_t'])
+        for i, bb in enumerate(data.pyg_mols):
+            bb_pos = _rigids_t[i].apply(bb.pos)
+
+            x_t.append(bb_pos)
+            atom_types.append(bb.atom_types)
+        
+        atom_types = torch.cat(atom_types, dim=0)
+        x_t = torch.cat(x_t, dim=0)
+
+        # get edge_indices, to_jimages
+        edge_indices, to_jimages = self.get_edge_indices(lattice, atom_types, x_t)
+        
+        # https://pytorch-geometric.readthedocs.io/en/latest/notes/batching.html
+        data = Data(
+            coords=torch.Tensor(x_t),
+            atom_types=torch.LongTensor(atom_types),
+            lengths=torch.Tensor(lattice[:3]).view(1, -1),
+            angles=torch.Tensor(lattice[3:]).view(1, -1),
+            edge_index=torch.LongTensor(
+                edge_indices.T).contiguous(),  # shape (2, num_edges)
+            to_jimages=torch.LongTensor(to_jimages),
+            num_atoms=data.num_atoms,
+            num_bbs=data.num_components,
+            num_bonds=edge_indices.shape[0],
+            num_nodes=data.num_atoms,  # special attribute used for batching in pytorch geometric
+            t=torch.Tensor([t]),
+            rigids_0=torch.Tensor(rigids_0),
+            rigids_t=torch.Tensor(diff_feats_t['rigids_t']),
+            trans_score=torch.Tensor(diff_feats_t['trans_score']),
+            rot_score=torch.Tensor(diff_feats_t['rot_score']),
+            trans_score_scaling=torch.tensor(diff_feats_t['trans_score_scaling'], dtype=torch.float32),
+            rot_score_scaling=torch.tensor(diff_feats_t['rot_score_scaling'], dtype=torch.float32),
+        )
+
+        return data
+
+    def get_edge_indices(self, lattice, atom_types, coords, cutoff=None):
+
+        structure_t = Structure(
+            lattice=Lattice.from_parameters(*lattice), 
+            species=atom_types.numpy(), 
+            coords=coords.numpy(),
+            coords_are_cartesian=True,
+        )
+        try:
+            graph_t = StructureGraph.with_local_env_strategy(
+                structure_t, CrystalNN
+            )
+        except:
+            tmp = local_env.CrystalNN(distance_cutoffs=cutoff, x_diff_weight=-1, porous_adjustment=False, search_cutoff=10)
+            graph_t = StructureGraph.with_local_env_strategy(
+                structure_t, tmp) 
+            
+        edge_indices, to_jimages = [], []
+        for i, j, to_jimage in graph_t.graph.edges(data='to_jimage'):
+            edge_indices.append([j, i])
+            to_jimages.append(to_jimage)
+            edge_indices.append([i, j])
+            to_jimages.append(tuple(-tj for tj in to_jimage))
+
+        return np.array(edge_indices), np.array(to_jimages)
+
+class TrainSampler(data.Sampler):
+
+    def __init__(
+            self,
+            *,
+            data_conf,
+            dataset,
+            batch_size,
+            sample_mode,
+        ):
+        self._log = logging.getLogger(__name__)
+        self._data_conf = data_conf
+        self._dataset = dataset
+        self._data_csv = self._dataset.csv
+        self._dataset_indices = list(range(len(self._data_csv)))
+        self._data_csv['index'] = self._dataset_indices
+        self._batch_size = batch_size
+        self.epoch = 0
+        self._sample_mode = sample_mode
+        self.sampler_len = len(self._dataset_indices) * self._batch_size
+
+        if self._sample_mode in ['cluster_length_batch', 'cluster_time_batch']:
+            self._pdb_to_cluster = self._read_clusters()
+            self._max_cluster = max(self._pdb_to_cluster.values())
+            self._log.info(f'Read {self._max_cluster} clusters.')
+            self._missing_pdbs = 0
+            def cluster_lookup(pdb):
+                pdb = pdb.upper()
+                if pdb not in self._pdb_to_cluster:
+                    self._pdb_to_cluster[pdb] = self._max_cluster + 1
+                    self._max_cluster += 1
+                    self._missing_pdbs += 1
+                return self._pdb_to_cluster[pdb]
+            self._data_csv['cluster'] = self._data_csv['pdb_name'].map(cluster_lookup)
+            num_clusters = len(set(self._data_csv['cluster']))
+            self.sampler_len = num_clusters * self._batch_size
+            self._log.info(
+                f'Training on {num_clusters} clusters. PDBs without clusters: {self._missing_pdbs}'
+            )
+
+    def _read_clusters(self):
+        pdb_to_cluster = {}
+        with open(self._data_conf.cluster_path, "r") as f:
+            for i,line in enumerate(f):
+                for chain in line.split(' '):
+                    pdb = chain.split('_')[0]
+                    pdb_to_cluster[pdb.upper()] = i
+        return pdb_to_cluster
+
+    def __iter__(self):
+        if self._sample_mode == 'length_batch':
+            # Each batch contains multiple proteins of the same length.
+            sampled_order = self._data_csv.groupby('modeled_seq_len').sample(
+                self._batch_size, replace=True, random_state=self.epoch)
+            return iter(sampled_order['index'].tolist())
+        elif self._sample_mode == 'time_batch':
+            # Each batch contains multiple time steps of the same protein.
+            random.shuffle(self._dataset_indices)
+            repeated_indices = np.repeat(self._dataset_indices, self._batch_size)
+            return iter(repeated_indices)
+        elif self._sample_mode == 'cluster_length_batch':
+            # Each batch contains multiple clusters of the same length.
+            sampled_clusters = self._data_csv.groupby('cluster').sample(
+                1, random_state=self.epoch)
+            sampled_order = sampled_clusters.groupby('modeled_seq_len').sample(
+                self._batch_size, replace=True, random_state=self.epoch)
+            return iter(sampled_order['index'].tolist())
+        elif self._sample_mode == 'cluster_time_batch':
+            # Each batch contains multiple time steps of a protein from a cluster.
+            sampled_clusters = self._data_csv.groupby('cluster').sample(
+                1, random_state=self.epoch)
+            dataset_indices = sampled_clusters['index'].tolist()
+            repeated_indices = np.repeat(dataset_indices, self._batch_size)
+            return iter(repeated_indices.tolist())
+        else:
+            raise ValueError(f'Invalid sample mode: {self._sample_mode}')
+
+    def set_epoch(self, epoch):
+        self.epoch = epoch
+
+    def __len__(self):
+        return self.sampler_len
+
+
+# modified from torch.utils.data.distributed.DistributedSampler
+# key points: shuffle of each __iter__ is determined by epoch num to ensure the same shuffle result for each proccessor
+class DistributedTrainSampler(data.Sampler):
+    r"""Sampler that restricts data loading to a subset of the dataset.
+
+    modified from torch.utils.data.distributed import DistributedSampler
+
+    .. note::
+        Dataset is assumed to be of constant size and that any instance of it always
+        returns the same elements in the same order.
+
+    Args:
+        dataset: Dataset used for sampling.
+        num_replicas (int, optional): Number of processes participating in
+            distributed training. By default, :attr:`world_size` is retrieved from the
+            current distributed group.
+        rank (int, optional): Rank of the current process within :attr:`num_replicas`.
+            By default, :attr:`rank` is retrieved from the current distributed
+            group.
+        shuffle (bool, optional): If ``True`` (default), sampler will shuffle the
+            indices.
+        seed (int, optional): random seed used to shuffle the sampler if
+            :attr:`shuffle=True`. This number should be identical across all
+            processes in the distributed group. Default: ``0``.
+        drop_last (bool, optional): if ``True``, then the sampler will drop the
+            tail of the data to make it evenly divisible across the number of
+            replicas. If ``False``, the sampler will add extra indices to make
+            the data evenly divisible across the replicas. Default: ``False``.
+
+    .. warning::
+        In distributed mode, calling the :meth:`set_epoch` method at
+        the beginning of each epoch **before** creating the :class:`DataLoader` iterator
+        is necessary to make shuffling work properly across multiple epochs. Otherwise,
+        the same ordering will be always used.
+
+    Example::
+
+        >>> # xdoctest: +SKIP
+        >>> sampler = DistributedSampler(dataset) if is_distributed else None
+        >>> loader = DataLoader(dataset, shuffle=(sampler is None),
+        ...                     sampler=sampler)
+        >>> for epoch in range(start_epoch, n_epochs):
+        ...     if is_distributed:
+        ...         sampler.set_epoch(epoch)
+        ...     train(loader)
+    """
+
+    def __init__(self, 
+                *,
+                data_conf,
+                dataset,
+                batch_size,
+                num_replicas: Optional[int] = None,
+                rank: Optional[int] = None, shuffle: bool = True,
+                seed: int = 0, drop_last: bool = False) -> None:
+        if num_replicas is None:
+            if not dist.is_available():
+                raise RuntimeError("Requires distributed package to be available")
+            num_replicas = dist.get_world_size()
+        if rank is None:
+            if not dist.is_available():
+                raise RuntimeError("Requires distributed package to be available")
+            rank = dist.get_rank()
+        if rank >= num_replicas or rank < 0:
+            raise ValueError(
+                "Invalid rank {}, rank should be in the interval"
+                " [0, {}]".format(rank, num_replicas - 1))
+        self._data_conf = data_conf
+        self._dataset = dataset
+        self._data_csv = self._dataset.csv
+        self._dataset_indices = list(range(len(self._data_csv)))
+        self._data_csv['index'] = self._dataset_indices
+        # _repeated_size is the size of the dataset multiply by batch size
+        self._repeated_size = batch_size * len(self._data_csv)
+        self._batch_size = batch_size
+        self.num_replicas = num_replicas
+        self.rank = rank
+        self.epoch = 0
+        self.drop_last = drop_last
+        # If the dataset length is evenly divisible by # of replicas, then there
+        # is no need to drop any data, since the dataset will be split equally.
+        if self.drop_last and self._repeated_size % self.num_replicas != 0:  # type: ignore[arg-type]
+            # Split to nearest available length that is evenly divisible.
+            # This is to ensure each rank receives the same amount of data when
+            # using this Sampler.
+            self.num_samples = math.ceil(
+                (self._repeated_size - self.num_replicas) / self.num_replicas  # type: ignore[arg-type]
+            )
+        else:
+            self.num_samples = math.ceil(self._repeated_size / self.num_replicas)  # type: ignore[arg-type]
+        self.total_size = self.num_samples * self.num_replicas
+        self.shuffle = shuffle
+        self.seed = seed
+
+    def __iter__(self) :
+        if self.shuffle:
+            # deterministically shuffle based on epoch and seed
+            g = torch.Generator()
+            g.manual_seed(self.seed + self.epoch)
+            indices = torch.randperm(len(self._data_csv), generator=g).tolist()  # type: ignore[arg-type]
+        else:
+            indices = list(range(len(self._data_csv)))  # type: ignore[arg-type]
+
+        # indices is expanded by self._batch_size times
+        indices = np.repeat(indices, self._batch_size)
+        
+        if not self.drop_last:
+            # add extra samples to make it evenly divisible
+            padding_size = self.total_size - len(indices)
+            if padding_size <= len(indices):
+                indices = np.concatenate((indices, indices[:padding_size]), axis=0)
+            else:
+                indices = np.concatenate((indices, np.repeat(indices, math.ceil(padding_size / len(indices)))[:padding_size]), axis=0)
+
+        else:
+            # remove tail of data to make it evenly divisible.
+            indices = indices[:self.total_size]
+        assert len(indices) == self.total_size
+
+        # subsample
+        indices = indices[self.rank:self.total_size:self.num_replicas]
+        # 
+        assert len(indices) == self.num_samples
+
+        return iter(indices)
+
+    def __len__(self) -> int:
+        return self.num_samples
+
+    def set_epoch(self, epoch: int) -> None:
+        r"""
+        Sets the epoch for this sampler. When :attr:`shuffle=True`, this ensures all replicas
+        use a different random ordering for each epoch. Otherwise, the next iteration of this
+        sampler will yield the same ordering.
+
+        Args:
+            epoch (int): Epoch number.
+        """
+        self.epoch = epoch
